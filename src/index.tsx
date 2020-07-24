a5bf07a5cdbf20a14083e6faea459d8558b40603
calvin <puram.calvin@gmail.com>
2020-07-24 16:25:02 +0100

    set the max height of the app

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className='bg-primary-200 p-0 m-0 box-border font-Spartan'>
+    <div className='bg-primary-200 min-h-screen p-0 m-0 box-border font-Spartan'>
       <App />
     </div>
   </React.StrictMode>,
